e9929a1986673f5e36c1291ab8d7724b275a6dbd
Andrew Symonds <notafan56@gmail.com>
2024-10-31 21:36:40 -0400

    My neovim profile

---
+++ lua/randrew/lazy/init.lua
@@ -0,0 +1,16 @@
+return {
+
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    "eandrju/cellular-automaton.nvim",
+
+    {
+    "ThePrimeagen/harpoon",
+    branch = "master",
+    dependencies = { "nvim-lua/plenary.nvim" }
+    }
+}
+
